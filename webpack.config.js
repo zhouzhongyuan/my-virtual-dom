c27abb41e908d9e230affcbdeea4f7d0f8d093cf
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-08-02 16:34:00 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const PATH = 'dist';
+const DEBUG = true;
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, PATH),
+        filename: 'bundle.js',
+    },
+    cache: DEBUG,
+    debug: DEBUG,
+    devtool: DEBUG ? 'eval-source-map' : false,
+    module: {
+        loaders: [
+            { test: /\.jsx?$/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react'],
+                },
+                plugins: ['transform-runtime'],
+            },
+        ],
+    },
+};
